e1f175a7ab3ea05b9a84f61fcd951c57d8688337
sapdotten <semenova.a.p.04@gmail.com>
2025-10-05 04:03:38 +0400

    test: add fixture and tests

---
+++ Library/Tests/Data/FixtureDataClass.cs
@@ -0,0 +1,334 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+
+namespace Tests.Data;
+
+/// <summary>
+/// Provides a comprehensive set of test data fixtures for the Library domain entities.
+/// This class is intended for use in unit tests, integration tests, and development scenarios 
+/// where consistent and realistic sample data is required.
+/// </summary>
+public class FixtureDataClass
+{
+    /// <summary>
+    /// Collection of 11 books representing various genres, authors, and publisher types, 
+    /// including fiction, technical literature, and educational materials.
+    /// </summary>
+    public List<Book> Books =>
+    [
+        new Book
+        {
+            Id = 1,
+            InventoryNumber = 1001,
+            Code = "PEL101",
+            Authors = "Виктор Пелевин",
+            Title = "Чапаев и Пустота",
+            PublisherType = PublisherType.Commercial,
+            Publisher = Publisher.Eksmo
+        },
+        new Book
+        {
+            Id = 2,
+            InventoryNumber = 1002,
+            Code = "PEL102",
+            Authors = "Виктор Пелевин",
+            Title = "Ампир V",
+            PublisherType = PublisherType.Commercial,
+            Publisher = Publisher.AST
+        },
+        new Book
+        {
+            Id = 3,
+            InventoryNumber = 1003,
+            Code = "PEL103",
+            Authors = "Виктор Пелевин",
+            Title = "Трансгуманизм Inc.",
+            PublisherType = PublisherType.Commercial,
+            Publisher = Publisher.AdMarginem
+        },
+        new Book
+        {
+            Id = 4,
+            InventoryNumber = 1004,
+            Code = "STR104",
+            Authors = "Аркадий Стругацкий, Борис Стругацкий",
+            Title = "Пикник на обочине",
+            PublisherType = PublisherType.Commercial,
+            Publisher = Publisher.Eksmo
+        },
+        new Book
+        {
+            Id = 5,
+            InventoryNumber = 1005,
+            Code = "TAN105",
+            Authors = "Эндрю Таненбаум",
+            Title = "Архитектура компьютера",
+            PublisherType = PublisherType.Academic,
+            Publisher = Publisher.Binom
+        },
+        new Book
+        {
+            Id = 6,
+            InventoryNumber = 1006,
+            Code = "LAV106",
+            Authors = "Шандорв ЛаВей",
+            Title = "Библия Сатаны",
+            PublisherType = PublisherType.Commercial,
+            Publisher = Publisher.Veche
+        },
+        new Book
+        {
+            Id = 7,
+            InventoryNumber = 1007,
+            Code = "MUR107",
+            Authors = "Харуки Мураками",
+            Title = "1Q84",
+            PublisherType = PublisherType.Commercial,
+            Publisher = Publisher.AST
+        },
+        new Book
+        {
+            Id = 8,
+            InventoryNumber = 1008,
+            Code = "MAR108",
+            Authors = "Джордж Р.Р. Мартин",
+            Title = "Игра престолов",
+            PublisherType = PublisherType.Commercial,
+            Publisher = Publisher.AST
+        },
+        new Book
+        {
+            Id = 9,
+            InventoryNumber = 1009,
+            Code = "KRA109",
+            Authors = "Генрих Крамер, Яков Шпренгер",
+            Title = "Молот ведьм",
+            PublisherType = PublisherType.Academic,
+            Publisher = Publisher.Nauka
+        },
+        new Book
+        {
+            Id = 10,
+            InventoryNumber = 1010,
+            Code = "IRO110",
+            Authors = "Иродов И.Е.",
+            Title = "Задачник по физике",
+            PublisherType = PublisherType.Educational,
+            Publisher = Publisher.Mir
+        },
+        new Book
+        {
+            Id = 11,
+            InventoryNumber = 1011,
+            Code = "IBS111",
+            Authors = "Владимир Милованов, Андрей Гостев",
+            Title = "Практическая информационная безопасность",
+            PublisherType = PublisherType.Educational,
+            Publisher = Publisher.Binom
+        }
+    ];
+
+    /// <summary>
+    /// Collection of 10 reader records representing students, with realistic names, 
+    /// contact details, and registration dates for testing library operations.
+    /// </summary>
+    public List<Reader> Readers =>
+    [
+        new Reader
+            {
+                Id = 1,
+                FirstName = "Иван",
+                SecondName = "Алехин",
+                LastName = "Игоревич",
+                Address = "ул. Ленина, д. 15, кв. 42",
+                PhoneNumber = "+7 (903) 123-45-67",
+                RegistrationDate = new DateOnly(2025, 1, 15)
+            },
+            new Reader
+            {
+                Id = 2,
+                FirstName = "Александр",
+                SecondName = "Волков",
+                LastName = "Юрьевич",
+                Address = "пр. Победы, д. 8, кв. 112",
+                PhoneNumber = "+7 (915) 234-56-78",
+                RegistrationDate = new DateOnly(2025, 2, 10)
+            },
+            new Reader
+            {
+                Id = 3,
+                FirstName = "Никита",
+                SecondName = "Гришин",
+                LastName = "Павлович",
+                Address = "ул. Мира, д. 27, кв. 33",
+                PhoneNumber = "+7 (926) 345-67-89",
+                RegistrationDate = new DateOnly(2025, 2, 20)
+            },
+            new Reader
+            {
+                Id = 4,
+                FirstName = "Никита",
+                SecondName = "Домнин",
+                LastName = "Михайлович",
+                Address = "ш. Энтузиастов, д. 5, кв. 76",
+                PhoneNumber = "+7 (937) 456-78-90",
+                RegistrationDate = new DateOnly(2025, 3, 5)
+            },
+            new Reader
+            {
+                Id = 5,
+                FirstName = "Даниил",
+                SecondName = "Иванов",
+                LastName = "Александрович",
+                Address = "ул. Космонавтов, д. 11, кв. 29",
+                PhoneNumber = "+7 (905) 567-89-01",
+                RegistrationDate = new DateOnly(2025, 1, 30)
+            },
+            new Reader
+            {
+                Id = 6,
+                FirstName = "Дмитрий",
+                SecondName = "Коновалов",
+                LastName = "Сергеевич",
+                Address = "пр. Мира, д. 34, кв. 55",
+                PhoneNumber = "+7 (916) 678-90-12",
+                RegistrationDate = new DateOnly(2025, 3, 12)
+            },
+            new Reader
+            {
+                Id = 7,
+                FirstName = "Родион",
+                SecondName = "Маркелов",
+                LastName = "Алексеевич",
+                Address = "ул. Строителей, д. 7, кв. 18",
+                PhoneNumber = "+7 (927) 789-01-23",
+                RegistrationDate = new DateOnly(2025, 2, 25)
+            },
+            new Reader
+            {
+                Id = 8,
+                FirstName = "Андрей",
+                SecondName = "Панявкин",
+                LastName = "Сергеевич",
+                Address = "ул. Радужная, д. 4, кв. 63",
+                PhoneNumber = "+7 (936) 890-12-34",
+                RegistrationDate = new DateOnly(2025, 3, 18)
+            },
+            new Reader
+            {
+                Id = 9,
+                FirstName = "Матвей",
+                SecondName = "Пихуров",
+                LastName = "Алексеевич",
+                Address = "пр. Ленинский, д. 22, кв. 91",
+                PhoneNumber = "+7 (904) 901-23-45",
+                RegistrationDate = new DateOnly(2025, 3, 22)
+            },
+            new Reader
+            {
+                Id = 10,
+                FirstName = "Артём",
+                SecondName = "Шикунов",
+                LastName = "Дмитриевич",
+                Address = "ул. Трудовая, д. 19, кв. 5",
+                PhoneNumber = "+7 (915) 012-34-56",
+                RegistrationDate = new DateOnly(2025, 1, 20)
+            }
+    ];
+
+    /// <summary>
+    /// Collection of 11 loan records representing realistic book borrowing scenarios, 
+    /// including various issue dates, loan terms, and associations between readers and books.
+    /// </summary>
+    public List<LoanRecord> LoanRecords =>
+    [
+        new LoanRecord
+        {
+            Id = 1,
+            BookId = 1,
+            ReaderId = 1,
+            IssueDate = new DateOnly(2025, 3, 10),
+            LoanTerm = 14
+        },
+        new LoanRecord
+        {
+            Id = 2,
+            BookId = 5,
+            ReaderId = 2,
+            IssueDate = new DateOnly(2025, 2, 15),
+            LoanTerm = 21
+        },
+        new LoanRecord
+        {
+            Id = 3,
+            BookId = 11,
+            ReaderId = 3,
+            IssueDate = new DateOnly(2025, 4, 5),
+            LoanTerm = 14
+        },
+        new LoanRecord
+        {
+            Id = 4,
+            BookId = 8,
+            ReaderId = 4,
+            IssueDate = new DateOnly(2025, 3, 20),
+            LoanTerm = 14
+        },
+        new LoanRecord
+        {
+            Id = 5,
+            BookId = 10,
+            ReaderId = 5,
+            IssueDate = new DateOnly(2025, 1, 25),
+            LoanTerm = 21
+        },
+        new LoanRecord
+        {
+            Id = 7,
+            BookId = 11,
+            ReaderId = 7,
+            IssueDate = new DateOnly(2025, 4, 10),
+            LoanTerm = 14
+        },
+        new LoanRecord
+        {
+            Id = 8,
+            BookId = 4,
+            ReaderId = 8,
+            IssueDate = new DateOnly(2025, 2, 28),
+            LoanTerm = 14
+        },
+        new LoanRecord
+        {
+            Id = 9,
+            BookId = 3,
+            ReaderId = 9,
+            IssueDate = new DateOnly(2025, 3, 5),
+            LoanTerm = 14
+        },
+        new LoanRecord
+        {
+            Id = 10,
+            BookId = 7,
+            ReaderId = 10,
+            IssueDate = new DateOnly(2025, 4, 3),
+            LoanTerm = 14
+        },
+        new LoanRecord
+        {
+            Id = 11,
+            BookId = 2,
+            ReaderId = 1,
+            IssueDate = new DateOnly(2025, 4, 12),
+            LoanTerm = 14
+        },
+        new LoanRecord
+        {
+            Id = 12,
+            BookId = 10,
+            ReaderId = 2,
+            IssueDate = new DateOnly(2025, 3, 1),
+            LoanTerm = 21
+        }
+    ];
+}
---
+++ Library/Tests/UnitTest.cs
@@ -0,0 +1,191 @@
+﻿using System.Reflection.PortableExecutable;
+using Domain.Entities;
+using Domain.Enums;
+using Tests.Data;
+
+namespace Tests;
+
+public class UnitTest(FixtureDataClass testData) : IClassFixture<FixtureDataClass>
+{
+    /// <summary>
+    /// Gets the list of borrowed books, ordered alphabetically by title.
+    /// </summary>
+    [Fact]
+    public void GetBorrowedBooks()
+    {
+
+        var expected = new[]
+        {
+            "1Q84",
+            "Ампир V",
+            "Архитектура компьютера",
+            "Задачник по физике",
+            "Игра престолов",
+            "Пикник на обочине",
+            "Практическая информационная безопасность",
+            "Трансгуманизм Inc.",
+            "Чапаев и Пустота"
+
+        };
+
+        var actualBooks = testData.LoanRecords
+            .Join(
+            testData.Books,
+            lr => lr.BookId,
+            b => b.Id,
+            (lr, b) => b.Title
+            )
+            .Distinct()
+            .OrderBy(title => title)
+            .ToList();
+
+
+        Assert.Equal(expected, actualBooks);
+    }
+
+    /// <summary>
+    /// Gets the top 5 most active readers who borrowed the highest number of books.
+    /// </summary>
+    [Fact]
+    public void GetMostActiveReaders()
+    {
+        var expectedReaders = new[]
+        {
+            "Алехин Иван Игоревич",
+            "Волков Александр Юрьевич",
+            "Гришин Никита Павлович",
+            "Домнин Никита Михайлович",
+            "Иванов Даниил Александрович"
+        };
+        var actualReaders = testData.LoanRecords
+            .GroupBy(lr => lr.ReaderId)
+            .Select(g => new
+            {
+                ReaderId = g.Key,
+                Count = g.Count()
+            })
+            .OrderByDescending(x => x.Count)
+            .Take(5)
+            .Join(
+                testData.Readers,
+                result => result.ReaderId,
+                reader => reader.Id,
+                (result, reader) => $"{reader.SecondName} {reader.FirstName} {reader.LastName}"
+            )
+            .ToList();
+
+        Assert.Equal(expectedReaders, actualReaders);
+    }
+
+    /// <summary>
+    /// Gets readers who borrowed books for the longest loan period, ordered by full name (second name, first name, last name).
+    /// </summary>
+    [Fact]
+    public void GetReadersWithLongetsLoan()
+    {
+        var expectedReaders = new[]
+        {
+            "Волков Александр Юрьевич",
+            "Иванов Даниил Александрович"
+        };
+
+        var actualReaders = testData.LoanRecords
+            .Where(x => x.LoanTerm == testData.LoanRecords.Max(lr => lr.LoanTerm))
+            .Join(
+                testData.Readers,
+                lr => lr.ReaderId,
+                r => r.Id,
+                (lr, r) => new
+                {
+                    r.SecondName,
+                    r.FirstName,
+                    r.LastName
+                }
+            )
+            .Distinct()
+            .Select( r=> $"{r.SecondName} {r.FirstName} {r.LastName}")
+            .OrderBy(name => name)
+            .ToList();
+        Assert.Equal(expectedReaders, actualReaders);
+    }
+
+    /// <summary>
+    /// Gets the top 5 most popular publishers over the last year, based on the number of book loans.
+    /// </summary>
+    [Fact]
+    public void GetMostPopularPublisher()
+    {
+        var expectedPublishers = new[]{
+            Publisher.AST,
+            Publisher.Binom,
+            Publisher.Eksmo,
+            Publisher.Mir,
+            Publisher.AdMarginem
+        };
+        var oneYearAgo = new DateOnly(2024, 10, 5);
+
+        var topPublishers = testData.LoanRecords
+            .Where(lr => lr.IssueDate >= oneYearAgo)
+            .Join(
+                testData.Books,
+                lr => lr.BookId,
+                b => b.Id,
+                (lr, b) => b.Publisher
+               )
+            .GroupBy(p => p)
+            .Select(g => new
+                {
+                    Publisher = g.Key,
+                    Count = g.Count()
+                }
+            )
+            .OrderByDescending(x => x.Count)
+            .ThenBy(x => x.Publisher)
+            .Select(x => x.Publisher)
+            .Take(5)
+            .ToList();
+
+        Assert.Equal(expectedPublishers, topPublishers);
+
+    }
+
+    /// <summary>
+    /// Gets the top 5 least popular publishers over the last year, including those with zero loans, ordered by loan count and then by publisher name.
+    /// </summary>
+    [Fact]
+    public void GetLeastPopularPublisher()
+    {
+        var expectedPublishers = new[]{
+            Publisher.Veche,
+            Publisher.Nauka,
+            Publisher.AdMarginem,
+            Publisher.Eksmo,
+            Publisher.Mir
+        };
+        var oneYearAgo = new DateOnly(2024, 10, 5);
+
+        var topPublishers = Enum.GetValues<Publisher>()
+            .Select(publisher => new
+            {
+                Publisher = publisher,
+                Count = testData.LoanRecords
+                .Where(lr => lr.IssueDate >= oneYearAgo)
+                .Join(
+                    testData.Books,
+                    lr => lr.BookId,
+                    b => b.Id,
+                    (_, b) => b.Publisher
+                    )
+                .Count(bookPublisher => bookPublisher == publisher)
+            })
+            .OrderBy(x => x.Count)
+            .ThenBy(x => x.Publisher)
+            .Take(5)
+            .Select(X => X.Publisher)
+            .ToList();
+
+
+        Assert.Equal(expectedPublishers, topPublishers);
+
+    }
+}
