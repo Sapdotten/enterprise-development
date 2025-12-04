658fdc203a75d3dbfa122368720586e9bc3b9476
sapdotten <semenova.a.p.04@gmail.com>
2025-12-05 00:54:48 +0400

    refactor: remove extra method from DataInitializer

--- Library/Library.Infrastructure.Postgres/DataInitializer.cs
+++ Library/Library.Infrastructure.Postgres/DataInitializer.cs
@@ -1,5 +1,4 @@
 ﻿using Library.Domain.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace Library.Infrastructure.Postgres;
 
@@ -38,26 +37,5 @@ public static class DataInitializer
             context.LoanRecords.AddRange(SeedData.LoanRecords);
             context.SaveChanges();
         }
-    }
-
-    /// <summary>
-    /// Retrieves the maximum ID value from a list of entities.
-    /// Uses reflection to access the Id property.
-    /// Returns 0 if the list is empty or no valid ID is found.
-    /// </summary>
-    /// <typeparam name="T">The entity type, must have an Id property.</typeparam>
-    /// <param name="list">The list of entities to scan.</param>
-    /// <returns>The highest ID value in the list, or 0 if none.</returns>
-    private static int GetMaxId<T>(List<T> list) where T : class
-    {
-        if (list.Count == 0)
-            return 0;
-
-        var idProperty = typeof(T).GetProperty("Id") ?? throw new InvalidOperationException("The type does not contain a property Id");
-
-        var maxItem = list.MaxBy(x => idProperty.GetValue(x) as int? ?? 0);
-        var maxValue = idProperty.GetValue(maxItem);
-
-        return maxValue is int id ? id : 0;
     }
 }
